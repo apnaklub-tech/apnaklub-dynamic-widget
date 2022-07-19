181cd6a4d8be43858717e377acae040afbb63e80
chetanApnaklub <106668220+chetanApnaklub@users.noreply.github.com>
2022-07-19 22:08:43 +0530

    Assertions: inkwell_widget_parser.dart, limitedbox_widget_parser.dart, listtile_widget_parser.dart, offstage_widget_parser.dart, opacity_widget_parser.dart, overflowbox_widget_parser.dart, placeholder_widget_parser.dart, rotatedbox_widget_parser.dart

--- lib/dynamic_widget/basic/rotatedbox_widget_parser.dart
+++ lib/dynamic_widget/basic/rotatedbox_widget_parser.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_widget/assertions/assert_constants.dart';
 import 'package:dynamic_widget/dynamic_widget.dart';
 import 'package:flutter/widgets.dart';
 
@@ -6,7 +7,8 @@ import '../../new_widget_parser.dart';
 class RotatedBoxWidgetParser extends NewWidgetParser {
   @override
   void assertionChecks(Map<String, dynamic> map) {
-    // TODO: implement assertionChecks
+    typeAssertionDriver(map: map, attribute: 'quarterTurns', expectedType: TYPE_INT);
+    typeAssertionDriver(map: map, attribute: 'child', expectedType: TYPE_MAP);
   }
 
 
