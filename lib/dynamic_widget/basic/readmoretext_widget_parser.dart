7332578a8225b6dd8d5b72db64c2df09309f66f2
chetanApnaklub <106668220+chetanApnaklub@users.noreply.github.com>
2022-07-19 10:23:35 +0530

    readmoretext_widget_parser.dart assertions

--- lib/dynamic_widget/basic/readmoretext_widget_parser.dart
+++ lib/dynamic_widget/basic/readmoretext_widget_parser.dart
@@ -1,4 +1,5 @@
 import 'package:dynamic_widget/apnaklub_widgets/readmoretext.dart';
+import 'package:dynamic_widget/assertions/assert_constants.dart';
 import 'package:dynamic_widget/dynamic_widget.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,6 +10,22 @@ class ReadMoreTextParser extends NewWidgetParser {
   @override
   void assertionChecks(Map<String, dynamic> map) {
     // TODO: implement assertionChecks
+    typeAssertionDriver(map: map, attribute: 'data', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'trimExpandedText', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'trimCollapsedText', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'colorClickableText', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'trimLength', expectedType: TYPE_INT);
+    typeAssertionDriver(map: map, attribute: 'trimLines', expectedType: TYPE_INT);
+    typeAssertionDriver(map: map, attribute: 'trimMode', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'textAlign', expectedType: TYPE_STRING);
+    //typeAssertionDriver(map: map, attribute: 'delimiterStyle', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'moreStyle', expectedType: TYPE_MAP);
+    typeAssertionDriver(map: map, attribute: 'lessStyle', expectedType: TYPE_MAP);
+    typeAssertionDriver(map: map, attribute: 'textDirection', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'locale', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'textScaleFactor', expectedType: TYPE_DOUBLE);
+    typeAssertionDriver(map: map, attribute: 'semanticsLabel', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'style', expectedType: TYPE_MAP);
   }
 
   @override
@@ -59,6 +76,7 @@ class ReadMoreTextParser extends NewWidgetParser {
       trimMode: parseTrimMode(map['trimMode']),
       textAlign: parseTextAlign(map['textAlign']),
       // delimiter: ,
+      // TODO - @Surya, why map is getting assigned to TextStyle type param.
       delimiterStyle: map['delimiterStyle'],
       moreStyle: parseTextStyle(map['moreStyle']),
       lessStyle: parseTextStyle(map['lessStyle']),
