9abea350ceac955ccf6e3fd10d193bf8a6812e0f
chetanApnaklub <106668220+chetanApnaklub@users.noreply.github.com>
2022-07-19 21:09:32 +0530

    Assertions : align_widget, appbar, aspectRatio, baseline, card

--- lib/dynamic_widget/basic/align_widget_parser.dart
+++ lib/dynamic_widget/basic/align_widget_parser.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_widget/assertions/assert_constants.dart';
 import 'package:dynamic_widget/dynamic_widget.dart';
 import 'package:dynamic_widget/dynamic_widget/utils.dart';
 import 'package:flutter/widgets.dart';
@@ -5,6 +6,14 @@ import 'package:flutter/widgets.dart';
 import '../../new_widget_parser.dart';
 
 class AlignWidgetParser extends NewWidgetParser {
+  @override
+  void assertionChecks(Map<String, dynamic> map) {
+    typeAssertionDriver(map: map, attribute: 'alignment', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'widthFactor', expectedType: TYPE_DOUBLE);
+    typeAssertionDriver(map: map, attribute: 'heightFactor', expectedType: TYPE_DOUBLE);
+    typeAssertionDriver(map: map, attribute: 'child', expectedType: TYPE_MAP);
+  }
+
   @override
   Widget parse(Map<String, dynamic> map, BuildContext buildContext,
       EventListener? listener) {
@@ -41,9 +50,4 @@ class AlignWidgetParser extends NewWidgetParser {
 
   @override
   Type get widgetType => Align;
-
-  @override
-  void assertionChecks(Map<String, dynamic> map) {
-    // TODO: implement assertionChecks
-  }
 }
