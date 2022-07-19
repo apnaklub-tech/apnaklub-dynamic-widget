60e4ffad83fcdb7f91a21e0d91ab3263db275d23
chetanApnaklub <106668220+chetanApnaklub@users.noreply.github.com>
2022-07-19 10:26:04 +0530

    padding_widget_parser.dart assertions

--- lib/dynamic_widget/basic/padding_widget_parser.dart
+++ lib/dynamic_widget/basic/padding_widget_parser.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_widget/assertions/assert_constants.dart';
 import 'package:dynamic_widget/dynamic_widget.dart';
 import 'package:dynamic_widget/dynamic_widget/utils.dart';
 import 'package:flutter/widgets.dart';
@@ -7,7 +8,8 @@ import '../../new_widget_parser.dart';
 class PaddingWidgetParser extends NewWidgetParser {
   @override
   void assertionChecks(Map<String, dynamic> map) {
-    // TODO: implement assertionChecks
+    typeAssertionDriver(map: map, attribute: 'padding', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'child', expectedType: TYPE_MAP);
   }
 
   @override
