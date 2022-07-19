21a5c7f0d1b7da8347f6236efdcdbc1f1b3f5e6e
chetanApnaklub <106668220+chetanApnaklub@users.noreply.github.com>
2022-07-19 22:22:04 +0530

    Assertions: safearea_widget_parser.dart, scaffold_widget_parser.dart, selectabletext_widget_parser.dart

--- lib/dynamic_widget/basic/safearea_widget_parser.dart
+++ lib/dynamic_widget/basic/safearea_widget_parser.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_widget/assertions/assert_constants.dart';
 import 'package:dynamic_widget/dynamic_widget.dart';
 import 'package:dynamic_widget/dynamic_widget/utils.dart';
 import 'package:flutter/widgets.dart';
@@ -7,7 +8,13 @@ import '../../new_widget_parser.dart';
 class SafeAreaWidgetParser extends NewWidgetParser {
   @override
   void assertionChecks(Map<String, dynamic> map) {
-    // TODO: implement assertionChecks
+    typeAssertionDriver(map: map, attribute: 'left', expectedType: TYPE_BOOL);
+    typeAssertionDriver(map: map, attribute: 'right', expectedType: TYPE_BOOL);
+    typeAssertionDriver(map: map, attribute: 'top', expectedType: TYPE_BOOL);
+    typeAssertionDriver(map: map, attribute: 'bottom', expectedType: TYPE_BOOL);
+    typeAssertionDriver(map: map, attribute: 'minimum', expectedType: TYPE_STRING);
+    typeAssertionDriver(map: map, attribute: 'maintainBottomViewPadding', expectedType: TYPE_BOOL);
+    typeAssertionDriver(map: map, attribute: 'child', expectedType: TYPE_MAP);
   }
 
   @override
