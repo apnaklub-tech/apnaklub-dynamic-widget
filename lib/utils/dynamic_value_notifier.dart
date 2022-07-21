f1523f0d6d0983001bb172900e0f80cf734d0b46
chetanApnaklub <106668220+chetanApnaklub@users.noreply.github.com>
2022-07-21 21:45:17 +0530

    add more actions to dynamic_value_notifier.dart

--- lib/utils/dynamic_value_notifier.dart
+++ lib/utils/dynamic_value_notifier.dart
@@ -1,16 +1,67 @@
-
 import 'package:dynamic_widget/dynamic_widget/utils.dart';
 import 'package:flutter/material.dart';
 
-class DynamicValueNotifier extends ValueNotifier<Map<String, dynamic>>{
+class DynamicValueNotifier extends ValueNotifier<Map<String, dynamic>> {
   DynamicValueNotifier({Map<String, dynamic> value = const {}}) : super(value);
 
   bool updateValue(String key, dynamic value) {
-    try{
+    try {
       this.value[key] = value;
       notifyListeners();
       return true;
-    } catch(e) {
+    } catch (e) {
+      print(toWarning("ERROR: updateValue $e"));
+      return false;
+    }
+  }
+
+  bool updateMultiple(Map<String, dynamic> map) {
+    try {
+      map.forEach((key, value) {
+        if (this.value.containsKey(key)) {
+          this.value[key] = value;
+        }
+      });
+      notifyListeners();
+      return true;
+    } catch (e) {
+      print(toWarning("ERROR: updateMultiple $e"));
+      return false;
+    }
+  }
+
+  bool updateAndAddMultiple(Map<String, dynamic> map) {
+    try {
+      map.forEach((key, value) {
+        this.value[key] = value;
+      });
+      notifyListeners();
+      return true;
+    } catch (e) {
+      print(toWarning("ERROR: updateMultiple $e"));
+      return false;
+    }
+  }
+
+  bool remove(String key) {
+    try {
+      value.remove(key);
+      notifyListeners();
+      return true;
+    } catch (e) {
+      print(toWarning("ERROR: updateValue $e"));
+      return false;
+    }
+  }
+
+  bool removeMultiple(List<String> keys) {
+    try {
+      for (var key in keys) {
+        value.remove(key);
+      }
+      notifyListeners();
+      return true;
+    } catch (e) {
       print(toWarning("ERROR: updateValue $e"));
       return false;
     }
